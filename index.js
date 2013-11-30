1e9c305d6f03ec76298f72a7e8b4d04ddc55fe94
Peter de croos <peter.decroos@smartshoot.com>
2013-11-30 13:22:29 -0800

    added in callback

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-
+/*jslint indent: 2 */
 //var Server
-
 var engine = require('engine.io');
 var Rx = require('rx');
 
-var Reiki = function() {
-  /*insert code here */
-  
+/* we assume there is either a server option which will be an instance of
+* httpServer or a port which shall be a number
+*/
+var Reiki = function(options) {
+  'use strict';
+  // engine.io is an eventemitter so we create a stream around the connect
+  if (options.server) {
+    this.server = options.server;
+    engine.attach(this.server);
+    this._createConnectionStream();
 
+  } else if (options.port) {
+    this.server = engine;
+    engine.listen(options.port);
+    this._createConnectionStream();
+  } else {
+    throw new Error('must include a valid port number or httpServer instance!!');
+  }
 };
+
+Reiki.prototype = Object.create({});
+
+Reiki.prototype._createConnectionStream = function(server) {
+  'use strict';
+  //creates the on connection from the server
+  this.connectionStream = Rx.fromEvent(this.server, 'connection', function(socket) {
+    return socket;
+  });
+};
+
+
 
 
 
