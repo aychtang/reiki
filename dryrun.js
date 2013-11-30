e4d366f26cff713c9bbec001555d9d644575b62d
Peter de croos <peter.decroos@smartshoot.com>
2013-11-30 13:40:19 -0800

    added dryrun file

---
+++ dryrun.js
@@ -0,0 +1,14 @@
+/*jslint indent: 2 */
+
+var http = require('http');
+var Reiki = require('./index.js');
+
+var server = http.createServer(function() {
+  'use strict';
+  console.log('request logged');
+});
+
+var reikiServer = new Reiki({ server : server });
+
+console.log('lsitening on port 80');
+reikiServer.listen(80);
--- index.js
+++ index.js
@@ -6,18 +6,18 @@ var Rx = require('rx');
 /* we assume there is either a server option which will be an instance of
 * httpServer or a port which shall be a number
 */
-var Reiki = function(options) {
+var Reiki = module.exports = function(options) {
   'use strict';
   // engine.io is an eventemitter so we create a stream around the connect
   if (options.server) {
     this.server = options.server;
     engine.attach(this.server);
-    this._createConnectionStream();
+    //this._createConnectionStream();
 
   } else if (options.port) {
     this.server = engine;
     engine.listen(options.port);
-    this._createConnectionStream();
+    //this._createConnectionStream();
   } else {
     throw new Error('must include a valid port number or httpServer instance!!');
   }
@@ -25,10 +25,16 @@ var Reiki = function(options) {
 
 Reiki.prototype = Object.create({});
 
+Reiki.prototype.listen = function(portNumber) {
+  'use strict';
+  this._createConnectionStream();
+  this.server.listen(portNumber);
+};
+
 Reiki.prototype._createConnectionStream = function(server) {
   'use strict';
   //creates the on connection from the server
-  this.connectionStream = Rx.fromEvent(this.server, 'connection', function(socket) {
+  this.connectionStream = Rx.Node.fromEvent(this.server, 'connection', function(socket) {
     return socket;
   });
 };
