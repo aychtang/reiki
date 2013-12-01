daf3d51093124893358b8573e7c476e8b4412ef7
Peter de croos <peter.decroos@smartshoot.com>
2013-11-30 16:12:16 -0800

    ready to start plugging in ws code

--- mocks/engineio-server.js
+++ mocks/engineio-server.js
@@ -2,13 +2,15 @@
 var http = require('http');
 var Reiki = require('../lib.index.js');
 
-var server = http.createServer(function(req, res) {
+var server = function() {
   'use strict';
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('hello world');
-  res.end('');
-});
+  http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('hello world');
+    res.end('');
+  });
+};
 
-module.exports = new Reiki(server);
+module.exports = new Reiki(server());
 
 
