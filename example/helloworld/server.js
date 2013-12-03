79bf3199f3e3e1557eef99e19f50e6c2d6250217
Howard Tang <tangh@google.com>
2013-12-02 22:14:17 -0800

    Uses rxjs in client side example until reiki client side is setup. Adds new doc in readme.md.

--- example/helloworld/server.js
+++ example/helloworld/server.js
@@ -1,6 +1,8 @@
+// Reiki Example.
 var Reiki = require('../../index.js');
-
 var r = new Reiki(8080);
+
+// Create an event stream for all socket 'customMessage' events.
 var messageStream = r.createEventStream('customMessage');
 
 messageStream.subscribe(function(data) {
